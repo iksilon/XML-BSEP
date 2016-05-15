a0cb7f2a14989f0dfb7ec640181781ef039b2f0c
IsiSku <isika93@gmail.com>
2016-05-15 22:23:07 +0200

    .

--- X.509v3 certificate generator/src/gui/MainWindow.java
+++ X.509v3 certificate generator/src/gui/MainWindow.java
@@ -2,16 +2,27 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	private final Action action = new SwingAction_2();
+	private final Action action_1 = new SwingAction_3();
+	private final Action action_2 = new SwingAction_4();
+	private final Action action_3 = new SwingAction_5();
+	private final Action action_4 = new SwingAction_6();
+	private final Action action_5 = new SwingAction_7();
 
 	/**
 	 * Launch the application.
@@ -41,10 +52,101 @@ public class MainWindow extends JFrame {
 		
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
+		
+		JMenu mntmNew = new JMenu("New");
+		mnFile.add(mntmNew);
+		
+		JMenuItem mntmKeystore = mntmNew.add(action);
+		mntmKeystore.setText("Keystore");
+		
+		JMenuItem mntmCertificate = mntmNew.add(action_1);
+		mntmCertificate.setText("Certificate");
+		
+		JMenuItem mntmOpen = mnFile.add(action_2);
+		mntmOpen.setText("Open");
+		
+		JSeparator separator = new JSeparator();
+		mnFile.add(separator);
+		
+		JMenuItem mntmSave = mnFile.add(action_3);
+		mntmSave.setText("Save");
+		
+		JMenuItem mntmSaveAs = mnFile.add(action_4);
+		mntmSaveAs.setText("Save as...");
+		
+		JSeparator separator_1 = new JSeparator();
+		mnFile.add(separator_1);
+		
+		JMenuItem mntmExit = mnFile.add(action_5);
+		mntmExit.setText("Exit");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
 
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_1 extends AbstractAction {
+		public SwingAction_1() {
+			putValue(NAME, "SwingAction_1");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_2 extends AbstractAction {
+		public SwingAction_2() {
+			putValue(NAME, "SwingAction_2");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_3 extends AbstractAction {
+		public SwingAction_3() {
+			putValue(NAME, "SwingAction_3");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_4 extends AbstractAction {
+		public SwingAction_4() {
+			putValue(NAME, "SwingAction_4");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_5 extends AbstractAction {
+		public SwingAction_5() {
+			putValue(NAME, "SwingAction_5");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_6 extends AbstractAction {
+		public SwingAction_6() {
+			putValue(NAME, "SwingAction_6");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_7 extends AbstractAction {
+		public SwingAction_7() {
+			putValue(NAME, "SwingAction_7");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
