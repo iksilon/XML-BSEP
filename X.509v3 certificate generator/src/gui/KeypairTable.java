a7d455e60517216e75ffc26b5d39c70017035475
IsiSku <isika93@gmail.com>
2016-05-16 09:01:34 +0200

    Dorada GUIja

---
+++ X.509v3 certificate generator/src/gui/KeypairTable.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+public class KeypairTable extends JTable {
+
+	private static final long serialVersionUID = 158730540208618426L;
+
+	TableModel model;
+	
+	public KeypairTable() {
+		DefaultTableModel dtm = new DefaultTableModel();
+		dtm.addColumn("No.");
+		dtm.addColumn("");
+		
+	}
+	
+	public KeypairTable(TableModel model) {
+		this.model = model;
+	}
+
+}
--- X.509v3 certificate generator/src/gui/MainWindow.java
+++ X.509v3 certificate generator/src/gui/MainWindow.java
@@ -9,20 +9,32 @@ import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
 
+/**
+ * Main view of the application, extension of the {@link JFrame} class.
+ * This window shows a keystore file with all it's certificates.
+ *
+ */
 public class MainWindow extends JFrame {
 
+	private static final long serialVersionUID = 1198734643308937757L;
+	
 	private JPanel contentPane;
-	private final Action action = new SwingAction_2();
-	private final Action action_1 = new SwingAction_3();
-	private final Action action_2 = new SwingAction_4();
-	private final Action action_3 = new SwingAction_5();
-	private final Action action_4 = new SwingAction_6();
-	private final Action action_5 = new SwingAction_7();
+	private final Action actKeystore = new ActionKeystore();
+	private final Action actKeypair = new ActionKeypair();
+	private final Action actOpen = new ActionOpen();
+	private final Action actSave = new ActionSave();
+	private final Action actSaveAs = new ActionSaveAs();
+	private final Action actExit = new ActionExit();
+	private final Action actExportCertificate = new ActionExportCertificate();
+	private final Action actExportAll = new ActionExportAll();
 
 	/**
 	 * Launch the application.
@@ -56,95 +68,121 @@ public class MainWindow extends JFrame {
 		JMenu mntmNew = new JMenu("New");
 		mnFile.add(mntmNew);
 		
-		JMenuItem mntmKeystore = mntmNew.add(action);
+		JMenuItem mntmKeystore = mntmNew.add(actKeystore);
 		mntmKeystore.setText("Keystore");
 		
-		JMenuItem mntmCertificate = mntmNew.add(action_1);
-		mntmCertificate.setText("Certificate");
+		JMenuItem mntmKeypair = mntmNew.add(actKeypair);
+		mntmKeypair.setText("Keypair");
 		
-		JMenuItem mntmOpen = mnFile.add(action_2);
+		JMenuItem mntmOpen = mnFile.add(actOpen);
 		mntmOpen.setText("Open");
 		
-		JSeparator separator = new JSeparator();
-		mnFile.add(separator);
+		JSeparator sepFile1 = new JSeparator();
+		mnFile.add(sepFile1);
 		
-		JMenuItem mntmSave = mnFile.add(action_3);
+		JMenuItem mntmSave = mnFile.add(actSave);
 		mntmSave.setText("Save");
 		
-		JMenuItem mntmSaveAs = mnFile.add(action_4);
+		JMenuItem mntmSaveAs = mnFile.add(actSaveAs);
 		mntmSaveAs.setText("Save as...");
 		
-		JSeparator separator_1 = new JSeparator();
-		mnFile.add(separator_1);
+		JSeparator sepFile2 = new JSeparator();
+		mnFile.add(sepFile2);
 		
-		JMenuItem mntmExit = mnFile.add(action_5);
+		JMenuItem mntmExit = mnFile.add(actExit);
 		mntmExit.setText("Exit");
+		
+		JMenu mnTools = new JMenu("Tools");
+		menuBar.add(mnTools);
+		
+		JMenuItem mntmExportCertificate = mnTools.add(actExportCertificate);
+		mntmExportCertificate.setText("Export Certificate");
+		
+		JMenuItem mntmExportAll = mnTools.add(actExportAll);
+		mntmExportAll.setText("Export All");
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		KeypairTable keypairTable = new KeypairTable();
+		keypairTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		scrollPane.setViewportView(keypairTable);
 	}
 
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "SwingAction");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionKeystore extends AbstractAction {
+		private static final long serialVersionUID = 425412543121784713L;
+		public ActionKeystore() {
+			putValue(NAME, "Keystore");
+			putValue(SHORT_DESCRIPTION, "Create new keystore");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_1 extends AbstractAction {
-		public SwingAction_1() {
-			putValue(NAME, "SwingAction_1");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionKeypair extends AbstractAction {
+		private static final long serialVersionUID = -1411136323257319945L;
+		public ActionKeypair() {
+			putValue(NAME, "Keypair");
+			putValue(SHORT_DESCRIPTION, "Generate new keypair");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_2 extends AbstractAction {
-		public SwingAction_2() {
-			putValue(NAME, "SwingAction_2");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionOpen extends AbstractAction {
+		private static final long serialVersionUID = 340823143919984037L;
+		public ActionOpen() {
+			putValue(NAME, "Open");
+			putValue(SHORT_DESCRIPTION, "Open a keystore file");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_3 extends AbstractAction {
-		public SwingAction_3() {
-			putValue(NAME, "SwingAction_3");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionSave extends AbstractAction {
+		private static final long serialVersionUID = -4641089031850059072L;
+		public ActionSave() {
+			putValue(NAME, "Save");
+			putValue(SHORT_DESCRIPTION, "Save keystore");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_4 extends AbstractAction {
-		public SwingAction_4() {
-			putValue(NAME, "SwingAction_4");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionSaveAs extends AbstractAction {
+		private static final long serialVersionUID = 3925404848236570471L;
+		public ActionSaveAs() {
+			putValue(NAME, "Save As");
+			putValue(SHORT_DESCRIPTION, "Save keystore to a file");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_5 extends AbstractAction {
-		public SwingAction_5() {
-			putValue(NAME, "SwingAction_5");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionExit extends AbstractAction {
+		private static final long serialVersionUID = 2732771330480399657L;
+		public ActionExit() {
+			putValue(NAME, "Exit");
+			putValue(SHORT_DESCRIPTION, "Close the application");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_6 extends AbstractAction {
-		public SwingAction_6() {
-			putValue(NAME, "SwingAction_6");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionExportCertificate extends AbstractAction {
+		private static final long serialVersionUID = -1698079888963949279L;
+		public ActionExportCertificate() {
+			putValue(NAME, "Export Certificate");
+			putValue(SHORT_DESCRIPTION, "Export keypair to a certificate file");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	private class SwingAction_7 extends AbstractAction {
-		public SwingAction_7() {
-			putValue(NAME, "SwingAction_7");
-			putValue(SHORT_DESCRIPTION, "Some short description");
+	private class ActionExportAll extends AbstractAction {
+		private static final long serialVersionUID = 5683267289392412616L;
+		public ActionExportAll() {
+			putValue(NAME, "Export All");
+			putValue(SHORT_DESCRIPTION, "Export all certificates to a specified folder");
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
