373aefaea5f4ee5327888279e18ee9ff74abd039
Lazar Andjelic <cyber.laki@gmail.com>
2016-05-29 14:06:50 +0200

    zasto se sve ovo nije komitovalo???

---
+++ BackendApp/busepBackend/public/angular/controllers/webAdmin.js
@@ -0,0 +1,22 @@
+(function() {
+	var app = angular.module('mainApp');
+	
+	app.controller('WebAdminCtrl', function($scope, $rootScope, $http) {
+		$scope.newUser = {email:'', role:''};
+		$scope.userRoles = ['Predsednik', 'Odbornik', 'Web admin'];
+		
+		$scope.addUser = function() {
+			$http.post('/user/create', {user:$scope.newUser})
+				.then(
+						function(response) {
+							//dodat korisnik
+						},
+						function(reason) {
+							console.log(reason.data);
+						});
+		};
+		$scope.deleteUser = function() {
+			
+		}
+	});
+}());
