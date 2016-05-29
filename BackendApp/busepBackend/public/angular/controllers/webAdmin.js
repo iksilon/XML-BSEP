b4273ee58feed5586901d4b8d68acf88d6d3e576
Lazar <cyber.laki@gmail.com>
2016-05-29 14:20:14 +0200

    Update webAdmin.js
    Update zbog addUser.html

--- BackendApp/busepBackend/public/angular/controllers/webAdmin.js
+++ BackendApp/busepBackend/public/angular/controllers/webAdmin.js
@@ -2,7 +2,7 @@
 	var app = angular.module('mainApp');
 	
 	app.controller('WebAdminCtrl', function($scope, $rootScope, $http) {
-		$scope.newUser = {email:'', role:''};
+		$scope.newUser = {email:'', role:'', pass:'', passConfirm:''};
 		$scope.userRoles = ['Predsednik', 'Odbornik', 'Web admin'];
 		
 		$scope.addUser = function() {
