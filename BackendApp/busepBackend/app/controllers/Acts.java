41da4002801c8b235313c2b2f46e4039c98c15c6
IsiSku <isika93@gmail.com>
2016-06-06 22:28:32 +0200

    document sign 0.1 Private method for signing some given data. I will be used locally by the XML parser to generate signature from given digest. Since I have no idea just now how to get the user, private key retrieval is hardcoded.

--- BackendApp/busepBackend/app/controllers/Acts.java
+++ BackendApp/busepBackend/app/controllers/Acts.java
@@ -1,10 +1,88 @@
 package controllers;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.SignedObject;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
 import play.mvc.Controller;
-import play.mvc.results.*;
+import play.mvc.results.Ok;
+import play.mvc.results.Result;
 public class Acts extends Controller {
 	
 	public static Result newAct() {
 		return new Ok();
+	}
+	
+	/**
+	 * Method for transforming given digest into a signature.
+	 * The returned {@link SignedObject} contains both the original message and the signature.
+	 * Right now is hardcoded to one private key.
+	 * 
+	 * @param data - {@link String} presumably representing the document hash.
+	 * @return {@link SignedObject}
+	 */
+	private static SignedObject signTextDocument(String data) {
+		try {
+			String workingDir = System.getProperty("user.dir");
+			workingDir = Paths.get(workingDir, "app", "keystores").toString();
+			
+			// TODO: Signing: Hardcoded keystore for now.
+			String filepath = Paths.get(workingDir, "odbornik1.jks").toString();
+			String password = "odbornik1";
+			
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(filepath));
+			KeyStore keystore = KeyStore.getInstance("JKS", "SUN");;
+			keystore.load(in, password.toCharArray());
+			
+			// TODO: Signing: Hardcoded private key for now.
+			PrivateKey pk = (PrivateKey) keystore.getKey(password, password.toCharArray());
+			
+			Signature sig = Signature.getInstance("SHA1withRSA");
+			SignedObject so = new SignedObject(data, pk, sig);
+			
+			return so;
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (CertificateException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (UnrecoverableKeyException e) {
+			e.printStackTrace();
+			return null;
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SignatureException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
