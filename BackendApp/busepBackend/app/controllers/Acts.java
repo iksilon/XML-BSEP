f885c58615acde289357967380f1dd0d53d1e083
IsiSku <isika93@gmail.com>
2016-06-07 22:37:16 +0200

    signature Set to /xml/submit route. Ready for testing when frontend is ready. Works with hardcoded placeholder.xml file. Creates signature.xml file. Awaiting XML feed from the front.

--- BackendApp/busepBackend/app/controllers/Acts.java
+++ BackendApp/busepBackend/app/controllers/Acts.java
@@ -1,8 +1,21 @@
 package controllers;
 
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+
+import org.w3c.dom.Document;
+
+import play.libs.XML;
 import play.mvc.Controller;
 import play.mvc.results.Ok;
 import play.mvc.results.Result;
+import utils.SecurityUtils;
+import utils.XMLUtils;
 public class Acts extends Controller {
 	
 	public static Result newAct() {
@@ -10,10 +23,36 @@ public class Acts extends Controller {
 	}
 	
 	
-	public static void receiveXML() {
+	public static void submitXML() {
 		// TODO: XML will be sent from the editor.
-				
+		String workingDir = System.getProperty("user.dir");
+		workingDir = Paths.get(workingDir, "public").toString();
 		
+		String sorcPath = Paths.get(workingDir, "placeholder.xml").toString();
+		String destPath = Paths.get(workingDir, "signature.xml").toString();
+		
+		Document xmlDoc = XMLUtils.loadDocument(sorcPath);
+		
+		// TODO: User will be extracted.
+		String kp = "odbornik1";
+		KeyStore ks = SecurityUtils.getKeyStore("odbornik1.jsk", kp.toCharArray());
+		
+		try {
+			PrivateKey pk = (PrivateKey) ks.getKey(kp, kp.toCharArray());
+			Certificate cert = ks.getCertificate(kp);
+			
+			Document signedDoc = SecurityUtils.signDocument(xmlDoc, pk, cert);
+			
+			//TODO: XML will be inserted into the database.
+			XMLUtils.saveDocument(signedDoc, destPath);
+			
+		} catch (UnrecoverableKeyException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
---
+++ BackendApp/busepBackend/app/utils/XMLUtils.java
@@ -0,0 +1,92 @@
+package utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XMLUtils {
+	
+	/**
+	 * Opens a XML document from file.
+	 * 
+	 * @param file - {@link String} of the full path to the XML file
+	 * @return {@link Document} DOM respresentation of the XML file 
+	 */
+	public static Document loadDocument(String file) {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			dbf.setNamespaceAware(true);
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document document = db.parse(new File(file));
+
+			return document;
+		} catch (FactoryConfigurationError e) {
+			e.printStackTrace();
+			return null;
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SAXException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Saves a XML file. 
+	 * 
+	 * @param doc - {@link Document} DOM representation
+	 * @param fileName - {@link String} filepath
+	 */
+	public static void saveDocument(Document doc, String fileName) {
+		try {
+			File outFile = new File(fileName);
+			FileOutputStream f = new FileOutputStream(outFile);
+
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer();
+			
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(f);
+			
+			transformer.transform(source, result);
+
+			f.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
