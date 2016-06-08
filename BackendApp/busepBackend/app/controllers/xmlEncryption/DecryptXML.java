7820f7b07b87d593189684c6de90d40e7d55bc69
Nemanja <nemanja.ilic@outlook.com>
2016-06-08 14:04:02 +0200

    [WIP] Dodata helper klasa za dekripciju XML

---
+++ BackendApp/busepBackend/app/controllers/xmlEncryption/DecryptXML.java
@@ -0,0 +1,105 @@
+package controllers.xmlEncryption;
+
+import org.apache.xml.security.encryption.XMLCipher;
+import org.apache.xml.security.encryption.XMLEncryptionException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+/**
+ * Created by Nemanja on 8/6/2016.
+ */
+public class DecryptXML {
+	static {
+		//staticka inicijalizacija
+		Security.addProvider(new BouncyCastleProvider());
+		org.apache.xml.security.Init.init();
+	}
+
+	/**
+	 * Ucitava privatni kljuc is KS fajla
+	 *
+	 * @param KEY_STORE_FILE putanja do keystore-a
+	 * @param alias Alias KeyStore-a
+	 * @param password Lozinka KeyStore-a
+	 */
+	private PrivateKey readPrivateKey(String KEY_STORE_FILE, String alias, String password) {
+		try {
+			//kreiramo instancu KeyStore
+			KeyStore ks = KeyStore.getInstance("JKS", "SUN");
+			//ucitavamo podatke
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(KEY_STORE_FILE));
+			ks.load(in, password.toCharArray());
+
+			if(ks.isKeyEntry(alias)) {
+				PrivateKey pk = (PrivateKey) ks.getKey(alias, password.toCharArray());
+				return pk;
+			}
+			else
+				return null;
+
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+			return null;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (CertificateException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (UnrecoverableKeyException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Kriptuje sadrzaj prvog elementa odsek
+	 */
+	private Document decrypt(Document doc, PrivateKey privateKey) {
+
+		try {
+			//cipher za dekritpovanje XML-a
+			XMLCipher xmlCipher = XMLCipher.getInstance();
+			//inicijalizacija za dekriptovanje
+			xmlCipher.init(XMLCipher.DECRYPT_MODE, null);
+			//postavlja se kljuc za dekriptovanje tajnog kljuca
+			xmlCipher.setKEK(privateKey);
+
+			//trazi se prvi EncryptedData element
+			//getElementsByTagNameNS(String namespaceURI, String localName)
+			//Returns a NodeList of all the Elements with a given local name and namespace URI in a document order
+			NodeList encDataList = doc.getElementsByTagNameNS("http://www.w3.org/2001/04/xmlenc#", "EncryptedData");
+			Element encData = (Element) encDataList.item(0);
+
+			//dekriptuje se
+			//pri cemu se prvo dekriptuje tajni kljuc, pa onda njime podaci
+			xmlCipher.doFinal(doc, encData);
+
+			return doc;
+		} catch (XMLEncryptionException e) {
+			e.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
