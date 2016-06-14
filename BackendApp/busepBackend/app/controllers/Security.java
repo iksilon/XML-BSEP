4028514f0757627231a0c2452f43141f5725e12e
Lazar Andjelic <cyber.laki@gmail.com>
2016-06-14 22:24:34 +0200

    Test dugme. Sastojci: sekund Lakijevog vremena Some more stuff

---
+++ BackendApp/busepBackend/app/controllers/Security.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import models.User;
+import play.mvc.results.Ok;
+import play.mvc.results.Result;
+
+public class Security extends Secure.Security {
+
+//	static boolean authenticate(String username, String password) {
+//		return Login.logIn3(username, password);
+//	}
+//	
+//	static Result login() {
+//		return User.find("byUsername", "admin").first();
+//	}
+//	
+//	static void onAuthenticated() {
+//		String user = Security.connected();
+//		
+//	}
+}
