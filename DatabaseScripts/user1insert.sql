55215fd34a00a8bcc9756661d190b2da17c0f9f9
IsiSku <isika93@gmail.com>
2016-06-14 16:19:42 +0200

    PLS USE SQL SCRIPT user1@email.com and their keystore added.

---
+++ DatabaseScripts/user1insert.sql
@@ -0,0 +1,20 @@
+USE [XML-BSEP]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([LAST_NAME]
+           ,[name]
+           ,[password]
+           ,[PASSWORD_SALT]
+           ,[username]
+           ,[role_id])
+     VALUES
+           (<NULL, varchar(150),>
+           ,<NULL, varchar(150),>
+           ,<bxi+3EEzP0wgWh6G93+by8sFVyU=, varchar(255),>
+           ,<1r7D9Liamag=, varchar(255),>
+           ,<user1@email.com, varchar(30),>
+           ,<2, numeric(19,0),>)
+GO
+
+
