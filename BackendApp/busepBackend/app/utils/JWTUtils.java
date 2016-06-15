8ee6e4aa1fbdc43ed63be058a0b84de1af0dcdf1
Lazar Andjelic <cyber.laki@gmail.com>
2016-06-15 20:28:57 +0200

    JWTUtils.java, forgot to commit them previously

---
+++ BackendApp/busepBackend/app/utils/JWTUtils.java
@@ -0,0 +1,149 @@
+package utils;
+
+import java.security.SecureRandom;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.jose4j.jwk.RsaJsonWebKey;
+import org.jose4j.jwk.RsaJwkGenerator;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.NumericDate;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.lang.JoseException;
+
+import models.User;
+
+public class JWTUtils {
+
+	private static ConcurrentHashMap<String, RsaJsonWebKey> userJwk = new ConcurrentHashMap<String, RsaJsonWebKey>();
+	
+	public static String generateJWT(User user) {
+		try {
+			RsaJsonWebKey jwk = RsaJwkGenerator.generateJwk(2048);			
+			jwk.setKeyId(user.id.toString());
+			
+			userJwk.put(user.username, jwk);
+			
+			JwtClaims claims = new JwtClaims();
+			claims.setIssuer("SGNS");
+			claims.setAudience(user.username);
+			claims.setExpirationTimeMinutesInTheFuture(10);
+			claims.setGeneratedJwtId();
+		    claims.setNotBeforeMinutesInThePast(2);
+		    claims.setSubject(user.username + "_form");
+		    claims.setClaim("name",user.name);
+		    claims.setClaim("lastname",user.lastName);
+		    claims.setIssuedAtToNow();
+		    
+		    JsonWebSignature jws = new JsonWebSignature();
+		    jws.setPayload(claims.toJson());
+		    jws.setKey(jwk.getPrivateKey());
+		    jws.setKeyIdHeaderValue(jwk.getKeyId());
+		    jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+
+		    String jwt = jws.getCompactSerialization();
+		    
+		    return jwt;
+		} catch (JoseException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static String getAudience(String jwt, User user) {
+		JwtConsumer jwtConsumer = getConsumer(user);
+		if(jwtConsumer == null) {
+			return null;
+		}
+		
+		try
+	    {
+			JwtClaims claims = jwtConsumer.processToClaims(jwt);
+	        try {
+	        	if(claims.getIssuedAt().isOnOrAfter(NumericDate.now())
+	        			|| claims.getExpirationTime().isOnOrAfter(NumericDate.now())) {
+	        		return null;
+	        	}
+	        	
+				if(!(claims.getAudience().get(0).equals(user.username)
+					&& claims.getIssuer().equals("SGNS")
+					&& claims.getSubject().equals(user.username + "_form")
+					&& claims.getClaimValue("name").equals(user.name)
+					&& claims.getClaimValue("lastname").equals(user.lastName))) {
+					return null;
+				}
+				
+				return claims.getAudience().get(0);
+			} catch (MalformedClaimException e) {
+		        System.out.println("Invalid Claim! " + e);
+			}
+	    }
+	    catch (InvalidJwtException e)
+	    {
+	        System.out.println("Invalid JWT! " + e);
+	    }
+		
+		return "";
+	}
+	
+	public static boolean checkJWT(String jwt, User user) {
+		JwtConsumer jwtConsumer = getConsumer(user);
+		if(jwtConsumer == null) {
+			return false;
+		}
+		
+		try
+	    {
+	        JwtClaims claims = jwtConsumer.processToClaims(jwt);
+	        try {
+	        	if(claims.getIssuedAt().isOnOrAfter(NumericDate.now())) {
+	        		return false;
+	        	}
+	        	
+				if(!(claims.getAudience().get(0).equals(user.username)
+					&& claims.getIssuer().equals("SGNS")
+					&& claims.getSubject().equals(user.username + "_form")
+					&& claims.getClaimValue("name").equals(user.name)
+					&& claims.getClaimValue("lastname").equals(user.lastName))) {
+					return false;
+				}
+				
+				System.out.println("JWT validation succeeded! " + claims);
+				return true;
+			} catch (MalformedClaimException e) {
+		        System.out.println("Invalid Claim! " + e);
+			}
+	    }
+	    catch (InvalidJwtException e)
+	    {
+	        System.out.println("Invalid JWT! " + e);
+	    }
+		
+		return false;
+	}
+	
+	private static JwtConsumer getConsumer(User user) {
+		RsaJsonWebKey jwk = userJwk.get(user.username);
+		
+		if(jwk == null) {
+			return null;
+		}
+		
+		JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+				.setRequireExpirationTime()
+				.setMaxFutureValidityInMinutes(300)
+				.setAllowedClockSkewInSeconds(30)
+				.setRequireSubject()
+				.setExpectedIssuer("SGNS")
+				.setExpectedAudience(user.username)
+				.setVerificationKey(jwk.getKey())
+				.build();
+		
+		return jwtConsumer;
+	}
+}
